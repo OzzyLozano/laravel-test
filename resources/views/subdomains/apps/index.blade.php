a88b587508e224149da80d3502c446862b2f567b
Oziel Lozano <oziellozanouwu@gmail.com>
2025-02-15 18:04:37 -0600

    named apps subdomain

--- resources/views/subdomains/apps/index.blade.php
+++ resources/views/subdomains/apps/index.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <title>Document</title>
+  <title>Apps</title>
 </head>
 <body>
   <h2>apps subdomain</h2>

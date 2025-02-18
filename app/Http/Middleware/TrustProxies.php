c2010a21595ac2f67d0af3cb4924fb1779d89d45
Oziel Lozano <oziellozanouwu@gmail.com>
2025-02-18 00:35:54 -0600

    added kernel & middleware

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,24 @@
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ForceHttps
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // Forzar HTTPS si no es en entorno local
+        if (!$request->secure() && env('APP_ENV') !== 'local') {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}

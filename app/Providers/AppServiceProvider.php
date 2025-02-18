35d2aa3bcae380f001de24c945b5ce93fb42d1ef
Oziel Lozano <oziellozanouwu@gmail.com>
2025-02-18 00:55:27 -0600

    appservice force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,26 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider {
-    /**
-     * Register any application services.
-     */
-    public function register(): void {
-        //
-    }
-
+class AppServiceProvider extends ServiceProvider
+{
     /**
      * Bootstrap any application services.
      */
-  public function boot(): void {
-      if (env('APP_ENV') === 'production') {
-        URL::forceScheme('https');
+    public function boot(): void
+    {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
-  }
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
 }

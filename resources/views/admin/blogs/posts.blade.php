69ea4dc47287eeeada06b3003477631fbb3ec643
Oziel Lozano <oziellozanouwu@gmail.com>
2025-02-19 00:54:22 -0600

    prueba5 detallitos

--- resources/views/admin/blogs/posts.blade.php
+++ resources/views/admin/blogs/posts.blade.php
@@ -8,17 +8,21 @@
 
 @section('content')
   <h1>List of Posts</h1>
-  <a href="{{ route('blogs.create') }}" class="btn btn-primary">Create New Post</a>
+  @if (auth()->check())
+    <a href="{{ route('blogs.create') }}" class="btn btn-primary">Create New Post</a>
+  @endif
   <ul>
     @foreach ($posts as $post)
       <li>
         <a href="{{ route('blogs.show', $post->id) }}">{{ $post->title }}</a>
-        <a href="{{ route('blogs.edit', $post->id) }}" class="btn btn-secondary">Edit</a>
-        <form action="{{ route('blogs.destroy', $post->id) }}" method="POST" style="display:inline;">
-          @csrf
-          @method('DELETE')
-          <button type="submit" class="btn btn-danger">Delete</button>
-        </form>
+        @if (auth()->check())
+          <a href="{{ route('blogs.edit', $post->id) }}" class="btn btn-secondary">Edit</a>
+          <form action="{{ route('blogs.destroy', $post->id) }}" method="POST" style="display:inline;">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger">Delete</button>
+          </form>
+        @endif
       </li>
     @endforeach
   </ul>
